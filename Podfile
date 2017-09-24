6ef2229320f6fdbae30cf156e3b8b645d1c36357
Connor Smith <cmds4410@gmail.com>
2017-09-24 16:58:12 -0700

    set up cocoapods

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'LightSound' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.5'
+
+end
---
+++ Pods/Target Support Files/Pods-LightSound/Pods-LightSound-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LightSound : NSObject
+@end
+@implementation PodsDummy_Pods_LightSound
+@end
